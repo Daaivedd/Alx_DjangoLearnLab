d6698a1bee9febb0aea09cf07d87829c51e1a96a
David Hanna <davidsamy955@gmail.com>
2024-08-29 03:55:06 +0300

    Django Views and URL Configuration Solution2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView 
-from .models import *
+from .models import Book, Library
 # Create your views here.
 def list_of_books (request, *args, **kwargs):
      books = Book.objects.all()
@@ -9,4 +9,4 @@ def list_of_books (request, *args, **kwargs):
 
 class library_of_detail (ListView):
     queryset = Library.objects.all()
-    template_name = "templates/library_detail.html"
+    template_name = "relationship_app/library_detail.html"
