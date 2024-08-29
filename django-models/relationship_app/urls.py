a47ac746e5620cf8ed0c50ad267f3dd4e86570e7
David Hanna <davidsamy955@gmail.com>
2024-08-29 04:29:24 +0300

    Django Views and URL Configuration Solution4

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_of_books, library_of_detail
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [ 
     path ('books/', list_of_books)
